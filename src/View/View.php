e61bd0367eca9d307c6243983bbb1292d23a5b61
SunshineDogs <57421132+SunshineDogs@users.noreply.github.com>
2020-05-04 13:14:36 +0300

    Update View.php

--- src/View/View.php
+++ src/View/View.php
@@ -21,29 +21,12 @@ class View
         echo ($code);
         ob_start();
 
-        include $this->templatesPath . '/' . $templateName;
+       include $this->templatesPath . DIRECTORY_SEPARATOR . $templateName;
         $buffer = ob_get_contents();
         ob_end_clean();
 
         echo $buffer;
 
     }
-    public function renderHtmlforZadach(string $templateName, $vars1, $vars2, $vars3)
-    {
 
-    http_response_code($code);
-    extract($vars1);
-    extract($vars2);
-    extract($vars3);
-    //extract($lay);
-  //  extract($lay2);
-    ob_start();
-
-    include $this->templatesPath . '/' . $templateName;
-    $buffer = ob_get_contents();
-    ob_end_clean();
-
-    echo $buffer;
-
-    }
 }
