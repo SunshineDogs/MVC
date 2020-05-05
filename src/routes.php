73ec48109125d8d311ceca7f975e900a4e0aaaf1
SunshineDogs <57421132+SunshineDogs@users.noreply.github.com>
2020-05-05 13:16:24 +0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -7,5 +7,4 @@ return [
     '~^auth~' => [\Controllers\AuthorizationController::class, 'main'],
     '~^logout~' => [\Controllers\AuthorizationController::class, 'logout'],
     '~^admin~' => [\Controllers\AuthorizationController::class, 'actionLogin'],
-    //[\Controllers\AuthorizationController::class, 'actionLogin'] => '~^$~',
 ];
