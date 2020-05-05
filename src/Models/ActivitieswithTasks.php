eab2f36bf2f2e8709e5597689d9e1c96b40a1af5
SunshineDogs <57421132+SunshineDogs@users.noreply.github.com>
2020-05-05 14:42:46 +0300

    Update ActivitieswithTasks.php

--- src/Models/ActivitieswithTasks.php
+++ src/Models/ActivitieswithTasks.php
@@ -8,29 +8,30 @@ class ActivitiesWithTasks
     public static function ShowAllTasks(): array
     {
         $db = new db();
+        
         return $db->query('SELECT * FROM `zadacha`;', [], self::class);
     }
+    
     public static function UpdateStatusOfTask($id_Task)
     {
         $db = new db();
-        //UPDATE MyGuests SET lastname='Doe' WHERE id=2
 
         return $db->query("UPDATE zadacha SET status=1 WHERE id = '$id_Task';", [], self::class);
     }
+    
     public static function UpdateTextOfTask($text,$id_Task)
     {
         $db = new db();
-        //UPDATE MyGuests SET lastname='Doe' WHERE id=2
 
         return $db->query("UPDATE zadacha SET text=? WHERE id = '$id_Task';", [$text], self::class);
     }
+    
     public static function UpdateRedbyAdmin($id_Task)
-{
-    $db = new db();
-    //UPDATE MyGuests SET lastname='Doe' WHERE id=2
-
-    return $db->query("UPDATE zadacha SET red_by_admin=1 WHERE id = '$id_Task';", [], self::class);
-}
+    {
+        $db = new db();
+    
+        return $db->query("UPDATE zadacha SET red_by_admin=1 WHERE id = '$id_Task';", [], self::class);
+    }
 
     public static function AddTask()
     {
@@ -38,18 +39,9 @@ class ActivitiesWithTasks
         $new_FIO = $_POST['user_name'];
         $new_email = $_POST['e-mail'];
         $new_text1 = $_POST['text'];
-
         $new_text2 = htmlspecialchars($new_text1);
         $new_text = strip_tags($new_text2);
 
         return $db->query("INSERT INTO `zadacha` VALUES (NULL,?,?,?,0,0);", [$new_FIO,$new_email,$new_text], self::class);
-
-
-        //echo addslashes($new_FIO);
-       // echo addslashes($new_email);
-       // $q->bind_param('s', $new_FIO);
-       // return $db->query("INSERT INTO `test` VALUES (?);", [$new_FIO], self::class);
-
-      //  return $db->query("INSERT INTO `zadacha` VALUES (NULL,'$new_FIO','$new_email','$new_text',NULL);", [], self::class);
     }
 }
