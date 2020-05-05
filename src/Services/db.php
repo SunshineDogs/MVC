b28ff0fd106b40dd85c9f6d4374efb36c0213124
SunshineDogs <57421132+SunshineDogs@users.noreply.github.com>
2020-05-05 13:29:08 +0300

    Update db.php

--- src/Services/db.php
+++ src/Services/db.php
@@ -4,27 +4,24 @@ namespace Services;
 
 class db
 {
+    
     private $pdo;
+    
     public function __construct()
     {
         $opt = require __DIR__ . '/../settings.php';
         $dbOptions = $opt['db'];
-        
-        $this->pdo = new \PDO(
-            "mysql:dbname={$dbOptions['dbname']};host={$dbOptions['host']}",
-            $dbOptions['user'],
-            $dbOptions['password']
-        );
-
+        $this->pdo = new \PDO("mysql:dbname={$dbOptions['dbname']};host={$dbOptions['host']}",$dbOptions['user'],$dbOptions['password']);
         $this->pdo->exec('SET NAMES UTF8');
     }
+    
     public function query(string $sql, $params = []): ?array
     {
         $sth = $this->pdo->prepare($sql);
         $result = $sth->execute($params);
 
         if (false === $result) {
-            return null;
+        	return null;
         }
 
         return $sth->fetchAll();
